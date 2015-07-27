7a0efeb2765896beb5d76f2a260ed72288d6213f
Anish Athalye <me@anishathalye.com>
2015-07-27 14:51:14 -0400

    Initial commit

---
+++ background.js
@@ -0,0 +1,25 @@
+var BASE_URL = 'https://go.hackmit.org/';
+
+function navigate(url) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.update(tabs[0].id, {url: url});
+  });
+}
+
+function golink(suffix) {
+  return BASE_URL + suffix;
+}
+
+chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+  // do nothing for now
+  // we'd need a special API for getting suggestions
+});
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+  var url = golink(text);
+  navigate(url);
+});
+
+chrome.omnibox.setDefaultSuggestion({
+  'description': 'Open ' + golink('%s')
+});
