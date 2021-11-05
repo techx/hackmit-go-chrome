90bcd9b80832fd11f6aea6321bc5889c92556a94
Kosi Nwabueze <kosinwabueze@gmail.com>
2021-11-05 02:26:07 -0400

    feat: made extension firefox compatible

--- background.js
+++ background.js
@@ -1,30 +1,27 @@
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
-  var BASE_URL = 'https://go.hackmit.org/';
+  var BASE_URL = "https://go.hackmit.org/";
 
   function navigate(url) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.update(tabs[0].id, {url: url});
-    });
+    browser.tabs.update({ url });
   }
 
   function golink(suffix) {
     return BASE_URL + suffix;
   }
 
-  chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+  browser.omnibox.onInputChanged.addListener(function (text, suggest) {
     // do nothing for now
     // we'd need a special API for getting suggestions
   });
 
-  chrome.omnibox.onInputEntered.addListener(function(text) {
+  browser.omnibox.onInputEntered.addListener(function (text) {
     var url = golink(text);
     navigate(url);
   });
 
-  chrome.omnibox.setDefaultSuggestion({
-    'description': 'Open ' + golink('%s')
+  browser.omnibox.setDefaultSuggestion({
+    description: "Open " + golink("%s"),
   });
-
-}());
+})();
